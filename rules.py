98883b1b47c8eadda18f9f95baec80f2c07e6038
Marsusss <marsus@live.dk>
2023-08-23 11:16:49 +0200

    chore: More linting

--- rules.py
+++ rules.py
@@ -3,6 +3,7 @@ import torch
 
 class rules:
     def __init__(self):
+        print("doing nothing")
 
     def is_move_valid(self, state, move):
         if move in self.get_allowed_moves(state):
@@ -39,10 +40,12 @@ class rules:
         return board_position
 
     def check_if_is_check(self, state, allowed_moves_coordinates, player, piece):
+        allowed_moves = self.coordinate_to_point(allowed_moves_coordinates)
         for i, coordinate in enumerate(allowed_moves_coordinates):
             suggested_board = state.board
             suggested_board[coordinate] = piece
-            suggested_board[from] = 0  # is empty
+            prev_position = [0, 0]  # This is a mock do it real
+            suggested_board[prev_position] = 0  # is empty
             check = self.is_check(self, suggested_board)
             if check:
                 allowed_moves[coordinate] = 0
@@ -69,25 +72,24 @@ class rules:
 
     def get_allowed_king_moves(self, state, player, position):
         own_board = state.get_player_positions(self, player)
-        base = torch.tensor([
-            [1, 1, 1],
-            [1, 0, 1],
-            [1, 1, 1]])
+        base = torch.tensor([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
         allowed_moves = torch.conv(base, self.coordinate_to_point(position))
         available_spaces = abs(own_board - 1)
         allowed_moves *= available_spaces
         allowed_moves_coordinates = torch.nonzero(allowed_moves)
-        allowed_moves = check_if_is_check(
-            self, state, allowed_moves_coordinates, player, 1)
+        allowed_moves = self.check_if_is_check(
+            self, state, allowed_moves_coordinates, player, 1
+        )
 
         return allowed_moves
 
-    def get_allowed_queen_moves(self, state, player, position, is_rook=False, is_bishop=False):
+    def get_allowed_queen_moves(
+        self, state, player, position, is_rook=False, is_bishop=False
+    ):
         own_positions = torch.nonzero(state.get_player_positions(self, player))
-        opponent_positions = torch.nonzero(
-            state.get_player_positions(self, player))
-        dist_xy = position, 7-position
+        opponent_positions = torch.nonzero(state.get_player_positions(self, player))
+        dist_xy = position, 7 - position
         allowed_moves = torch.zeros((8, 8))
         toggle_xp = True
         toggle_xm = True
@@ -127,80 +129,93 @@ class rules:
             if toggle_xp:
                 if xp <= 7:
                     toggle_xp, allowed_moves[pos_xp] = self.is_blocked(
-                        own_positions[pos_xp], opponent_positions[pos_xp])
+                        own_positions[pos_xp], opponent_positions[pos_xp]
+                    )
                 else:
                     toggle_xp = False
 
             if toggle_xm:
                 if xm >= 0:
                     toggle_xm, allowed_moves[pos_xm] = self.is_blocked(
-                        own_positions[pos_xm], opponent_positions[pos_xm])
+                        own_positions[pos_xm], opponent_positions[pos_xm]
+                    )
                 else:
                     toggle_xm = False
 
             if toggle_yp:
                 if yp <= 7:
                     toggle_yp, allowed_moves[pos_yp] = self.is_blocked(
-                        own_positions[pos_yp], opponent_positions[pos_yp])
+                        own_positions[pos_yp], opponent_positions[pos_yp]
+                    )
                 else:
                     toggle_yp = False
 
             if toggle_ym:
                 if ym >= 0:
                     toggle_ym, allowed_moves[pos_ym] = self.is_blocked(
-                        own_positions[pos_ym], opponent_positions[pos_ym])
+                        own_positions[pos_ym], opponent_positions[pos_ym]
+                    )
                 else:
                     toggle_ym = False
 
             if toggle_xpyp:
                 if xp <= 7 and yp <= 7:
                     toggle_xpyp, allowed_moves[pos_xpyp] = self.is_blocked(
-                        own_positions[pos_xpyp], opponent_positions[pos_xpyp])
+                        own_positions[pos_xpyp], opponent_positions[pos_xpyp]
+                    )
                 else:
                     toggle_xpyp = False
 
             if toggle_xpym:
                 if xp <= 7 and ym >= 0:
                     toggle_xpym, allowed_moves[pos_xpym] = self.is_blocked(
-                        own_positions[pos_xpym], opponent_positions[pos_xpym])
+                        own_positions[pos_xpym], opponent_positions[pos_xpym]
+                    )
                 else:
                     toggle_xpym = False
 
             if toggle_xmyp:
                 if xm >= 0 and yp <= 7:
                     toggle_xmyp, allowed_moves[pos_xmyp] = self.is_blocked(
-                        own_positions[pos_xmyp], opponent_positions[pos_xmyp])
+                        own_positions[pos_xmyp], opponent_positions[pos_xmyp]
+                    )
                 else:
                     toggle_xmyp = False
 
             if toggle_xmym:
                 if xm >= 0 and ym >= 0:
                     toggle_xmym, allowed_moves[pos_xmym] = self.is_blocked(
-                        own_positions[pos_xmym], opponent_positions[pos_xmym])
+                        own_positions[pos_xmym], opponent_positions[pos_xmym]
+                    )
                 else:
                     toggle_xmym = False
 
         allowed_moves_coordinates = torch.nonzero(allowed_moves)
-        allowed_moves = check_if_is_check(
-            self, state, allowed_moves_coordinates, player, 2)
+        allowed_moves = self.check_if_is_check(
+            self, state, allowed_moves_coordinates, player, 2
+        )
 
         return allowed_moves
 
     def get_allowed_rook_moves(self, state, player, position):
-        return get_allowed_queen_moves(self, state, player, position, is_rook=True)
+        return self.get_allowed_queen_moves(self, state, player, position, is_rook=True)
 
     def get_allowed_bishop_moves(self, state, player, position):
-        return get_allowed_queen_moves(self, state, player, position, is_bishop=True)
+        return self.get_allowed_queen_moves(
+            self, state, player, position, is_bishop=True
+        )
 
     def get_allowed_knight_moves(self, state, player, position):
         own_board = state.get_player_positions(self, player)
-        base = torch.tensor([
-            [0, 1, 0, 1, 0],
-            [1, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 1],
-            [0, 1, 0, 1, 0]
-        ])
+        base = torch.tensor(
+            [
+                [0, 1, 0, 1, 0],
+                [1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1],
+                [0, 1, 0, 1, 0],
+            ]
+        )
 
         allowed_moves = torch.conv(base, self.coordinate_to_point(position))
         available_spaces = abs(own_board - 1)
@@ -208,94 +223,104 @@ class rules:
         allowed_moves_coordinates = torch.nonzero(allowed_moves)
 
         allowed_moves_coordinates = torch.nonzero(allowed_moves)
-        allowed_moves = check_if_is_check(
-            self, state, allowed_moves_coordinates, player, 4)
+        allowed_moves = self.check_if_is_check(
+            self, state, allowed_moves_coordinates, player, 4
+        )
 
     def get_allowed_pawn_moves(self, state, player, position):
         allowed_moves = torch.zeros((8, 8))
         if player == -1:
             allowed_moves[position[0], position[1] + 1] = 1
-            if y_position == 1:
+            if position[1] == 1:
                 allowed_moves[position[0], position[1] + 2] = 1
 
         else:
-            if y_position == 6:
+            if position[1] == 6:
+                print("mock")
 
-    def get_queen_move_pattern():
-        return torch.tensor([
-            [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-            [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-            [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]
-        ])
+    def get_queen_move_pattern(self):
+        return torch.tensor(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+                [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+            ]
+        )
 
-    def get_rook_move_pattern():
-        return torch.tensor([
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-        ])
+    def get_rook_move_pattern(self):
+        return torch.tensor(
+            [
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
 
-    def get_bishop_move_pattern():
-        return torch.tensor([
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-        ])
+    def get_bishop_move_pattern(self):
+        return torch.tensor(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            ]
+        )
 
-    def get_knight_move_pattern():
-        return torch.tensor([
-            [0, 1, 0, 1, 0],
-            [1, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 1],
-            [0, 1, 0, 1, 0]
-        ])
+    def get_knight_move_pattern(self):
+        return torch.tensor(
+            [
+                [0, 1, 0, 1, 0],
+                [1, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 1],
+                [0, 1, 0, 1, 0],
+            ]
+        )
 
-    def get_pawn_move_pattern():
-        if state.get_player_color(player) == 0:
-            return torch.tensor([
-                [1, 1, 1],
-                [0, 0, 0],
-                [0, 0, 0]])
-
-        else:
-            return torch.tensor([
-                [0, 0, 0],
-                [0, 0, 0],
-                [1, 1, 1]])
+    # def get_pawn_move_pattern(self):
+    #     if state.get_player_color(player) == 0:
+    #         return torch.tensor([
+    #             [1, 1, 1],
+    #             [0, 0, 0],
+    #             [0, 0, 0]])
+    #
+    #     else:
+    #         return torch.tensor([
+    #             [0, 0, 0],
+    #             [0, 0, 0],
+    #             [1, 1, 1]])
